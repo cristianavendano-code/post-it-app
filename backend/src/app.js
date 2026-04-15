db8b2c3f51d0ddd14f709db2d5b5cd7eeeed416a
Cristian Avendaño del Solar <cristianavendano.dev@outlook.com>
2026-04-15 09:52:50 -0600

    Conexión MySQL y el servidor Node.js están acoplados correctamente

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
+const pool = require('./config/db');
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//SELECT 1 + 1 AS test
+async function testDBConnection() {
+  try {
+    const connection = await pool.getConnection();
+    const [rows] = await connection.query('SELECT 1 + 1 AS test');
+    console.log('Database connection successful:', rows);
+    connection.release();
+  } catch (error) {
+    console.error('Database connection failed:', error);
+  }
+}
+
+testDBConnection();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
